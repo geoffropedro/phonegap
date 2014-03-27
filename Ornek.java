9ed035759c21f3d6398dc5ff6302ea31e00b41e1
geoffropedro <geoff@pixperfect.co.uk>
2014-03-27 12:08:43 +0000

    Teessdddd

---
+++ Ornek.java
@@ -0,0 +1,14 @@
+package com.phonegap.ornek;
+
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class Ornek extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
